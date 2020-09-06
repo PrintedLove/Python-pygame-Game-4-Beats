8cd9880cbc1606d8ea8f52f42aff1d21e4f3643a
PrintedLove <PrintedLove@github.com>
2020-09-06 19:07:08 +0900

    2020-09-06

---
+++ README.md
@@ -0,0 +1,12 @@
+Made by Printed Love
+Blog: https://printed.tistory.com/
+
+
+Update log >
+
+2020.09.05 - Project Create
+
+
+
+
+convert to exe: pyinstaller --noconsole --icon=icon.ico start.py
--- start.py
+++ start.py
@@ -2,79 +2,83 @@
 # made by "PrintedLove"
 # https://printed.tistory.com/
 
+# This game was created with reference to ParkJuneWoo(korca0220)'s [Finding-the-Rabbit]
+
 import pygame as pg
-import random
 import time
 import os
 from sprites import *
 
+### default setting
 TITLE = "4 Beats"
 WIDTH = 640
 HEIGHT = 480
 FPS = 60
-FONT_NAME = 'neodgm.ttf'
+FONT_NAME = 'HeirofLightRegular.ttf'
+
+### color setting
+WHITE = (238, 238, 238)
+BLACK = (32, 36, 32)
+RED = (246, 36, 74)
+BLUE = (32, 105, 246)
+
 
 class Game:
-    def __init__(self): ########################## 게임 시작
+    def __init__(self): ########################## Game Start
         pg.init()
-        pg.mixer.init()     #사운드 믹서
-        pg.display.set_caption(TITLE)       #타이틀 이름
-        self.screen = pg.display.set_mode((WIDTH, HEIGHT))      #화면 사이즈 세팅
-        self.clock = pg.time.Clock()        #FPS 설정용 타이머
-        self.running = True     #게임 실행 Boolean 값
-        self.selecting = True       #메뉴 선택 Boolean 값
-        self.load_date()        #데이터 로딩 함수 실행
+        pg.mixer.init()     #sound mixer
+        pg.display.set_caption(TITLE)       #title name
+        pg.display.set_icon(pg.image.load(os.path.join(os.path.join(os.path.dirname(__file__), 'image'), 'icon.png')))
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))      #screen size
+        
+        self.clock = pg.time.Clock()        #FPS timer
+        self.running = True     #game initialize Boolean value
+        self.screen_mode = 0    #screen mode (0: logo, 1: main, 2: help, 3:stage select, 10: play)
+        self.load_date()        #data loading
 
-    def load_date(self): ########################## 파일 로딩
+    def load_date(self): ########################## Data Loading
         self.dir = os.path.dirname(__file__)
         
-        ### 폰트
+        ### font
         self.gameFont = os.path.join(os.path.join(self.dir, 'font'), FONT_NAME)
         
-        ### 이미지
+        ### image
         self.img_dir = os.path.join(self.dir, 'image')
         
-        self.spr_logoback = Spritesheet(os.path.join(self.img_dir, 'logoback.png'))
-        self.spr_logo = Spritesheet(os.path.join(self.img_dir, 'logo.png'))
-        self.spr_button_logo = Spritesheet(os.path.join(self.img_dir, 'button_logo.png'))
-        self.spr_buttonEffect_logo = Spritesheet(os.path.join(self.img_dir, 'buttonEffect_logo.png'))
+        pg.display.set_icon(pg.image.load(os.path.join(self.img_dir, 'icon.png')))      #set icon
+        self.spr_logoback = pg.image.load(os.path.join(self.img_dir, 'logoback.png'))
+        self.spr_logo = pg.image.load(os.path.join(self.img_dir, 'logo.png'))
+        self.spr_button_logo = pg.image.load(os.path.join(self.img_dir, 'button_logo.png'))
+        self.spr_buttonEffect_logo = pg.image.load(os.path.join(self.img_dir, 'buttonEffect_logo.png'))
 
-        ### 배경음악, 효과음
+        ### background music
         self.snd_dir = os.path.join(self.dir, 'sound')
         
-        self.sound_bg_main = pg.mixer.Sound(os.path.join(self.snd_dir, 'theaudioway.ogg'))
+        #self.sound_bg_main = pg.mixer.Sound(os.path.join(self.snd_dir, 'bg_main.ogg'))
         
-    def new(self):      ########################## 게임 초기화
-        ###변수 초기화
-        self.score = 0;     #점수
-        self.stage = 1;     #스테이지
-        self.circle_rot = 0;     #중심 회전값
+    def new(self):      ########################## Game Initialize
+        ### value initialize
+        self.stage = 1;     #stage
+        self.circle_rot = 0;     #circle rotation value
+        self.circle_dir = 0;     #circle direction value
 
-        ### 스프라이트 그룹
+        ### sprite group
         self.all_sprites = pg.sprite.Group()
         self.effects = pg.sprite.Group()
         self.bars = pg.sprite.Group()
 
-        ### 플레이 타임 타이머
+        ### playtime timer
         self.start_tick = pg.time.get_ticks()
         
-        ### 배경 음악 실행
-        pg.mixer.music.load(self.sound_bg_main)
+        ### bgm
+        pg.mixer.music.load(os.path.join(self.snd_dir, 'bg_main.ogg'))
 
-        ### 점수값 로드
-        with open(os.path.join(self.dir, 'highscore.txt'), 'r') as f:
-            try:
-                self.highscore = int(f.read())
-            except:
-                self.highscore = 0
-
-        ### 게임 루프 실행
+        ### gameloop start
         self.run()
         
-    def run(self):      ########################## 게임 루프
-        pg.mixer.music.play(loops = -1)
-        
+    def run(self):      ########################## Game Loop
         self.playing = True
+        pg.mixer.music.play(loops = -1)
 
         while self.playing:
             self.clock.tick(FPS)
@@ -84,32 +88,36 @@ class Game:
 
         pg.mixer.music.fadeout(600)
     
-    def update(self):   ########################## 게임 루프 - 업데이트
-        self.all_sprites.update()       #화면 업데이트
-        self.second = ((pg.time.get_ticks() - self.start_tick) / 1000)      #플레이 타임 계산
+    def update(self):   ########################## Game Loop - Update
+        self.all_sprites.update()       #screen update
+        self.second = ((pg.time.get_ticks() - self.start_tick) / 1000)      #play time calculation
         
-    def events(self):   ########################## 게임 루프 - 이벤트
+    def events(self):   ########################## Game Loop - Events
         for event in pg.event.get():
-            if event.type == pg.QUIT:       #종료
-                self.start = False
-                
+            if event.type == pg.QUIT:       #exit
                 if self.playing:
                     self.playing = False
                     self.running = False
         
-    def draw(self):     ########################## 게임 루프 - 드로우
+    def draw(self):     ########################## Game Loop - Draw
         self.all_sprites.draw(self.screen)
 
+        self.spr_logo.set_alpha(50)
         self.screen.blit(self.spr_logoback, (0, 0))
+        self.screen.blit(self.spr_logo, (320, 40))
         
         pg.display.update()
 
-g = Game()
+    def draw_text(self, text, size, color, x, y):   # Draw Text
+        font = pg.font.Font(self.gameFont, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
 
-while g.start:
-    #g.show_start_screen()
-    
-    while g.running:
-        g.new()
+game = Game()
+
+while game.running:
+    game.new()
             
 pg.quit()
